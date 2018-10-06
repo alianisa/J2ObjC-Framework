4c95b059da39d8f17dc6aa3cab285fc0ed41848d
aloim <32746943+aloim@users.noreply.github.com>
2018-10-06 21:12:17 +0500

    Update download.sh

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -4,7 +4,8 @@ set -ev
 j2objc_version=2.2
 #link=https://github.com/google/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
 link=http://localhost/j2objc-2.2.zip
-sha1_checksum=a62807929c2583a03cc73d57ce67fc5730cf24b7
+#sha1_checksum=a62807929c2583a03cc73d57ce67fc5730cf24b7
+sha1_checksum=d003004f9291bee9e7b0ffef8cc150f263d3af53
 
 echo "fetching j2objc dist"
 #curl -OL https://github.com/google/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
