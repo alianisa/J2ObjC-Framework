1a7d5059e7d919c67007e33522d8b23a7467ab77
aloim <32746943+aloim@users.noreply.github.com>
2019-09-22 14:01:14 +0500

    Update download.sh

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -3,7 +3,7 @@ set -ev
 
 j2objc_version=2.2
 #link=https://github.com/google/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
-link=http://localhost/j2objc-2.2.zip
+link=http://localhost/j2objc-2.5.zip
 #sha1_checksum=a62807929c2583a03cc73d57ce67fc5730cf24b7
 sha1_checksum=d003004f9291bee9e7b0ffef8cc150f263d3af53
 
