e93996afcc3ff6a2f44287f7cd7ad1ecd033446b
alianisa <32746943+alianisa@users.noreply.github.com>
2021-08-09 20:39:41 +0500

    Update download.sh

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -ev
 
-j2objc_version=2.5
-#link=https://github.com/google/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
-link=http://localhost/j2objc-2.5.zip
+j2objc_version=2.8
+link=https://github.com/google/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
+#link=http://localhost/j2objc-2.5.zip
 #sha1_checksum=a62807929c2583a03cc73d57ce67fc5730cf24b7
-sha1_checksum=39f5a54f0a9cfacb68948ea77408ea51b5f04f63
+#sha1_checksum=39f5a54f0a9cfacb68948ea77408ea51b5f04f63
 
 echo "fetching j2objc dist"
-#curl -OL https://github.com/google/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
-curl -OL http://localhost/j2objc-2.5.zip
-echo "${sha1_checksum}  j2objc-${j2objc_version}.zip" | shasum -c
+curl -OL https://github.com/google/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
+#curl -OL http://localhost/j2objc-2.5.zip
+#echo "${sha1_checksum}  j2objc-${j2objc_version}.zip" | shasum -c
 unzip -o -q j2objc-${j2objc_version}.zip
 mv j2objc-${j2objc_version} Distributive
 rm j2objc-${j2objc_version}.zip
